0a40ee2c532a88cbff7fd462eb28d37937f8005f
Prasanna Balaji <erprasanna97@gmail.com>
2019-12-11 13:49:34 +0530

    added scripts for add and remove items in field

--- MultiSelect-DropDown/resources/js/script.js
+++ MultiSelect-DropDown/resources/js/script.js
@@ -50,3 +50,72 @@ const fetchData = () => {
       });
   });
 };
+
+// add the selected items
+const add_selected_item = (elem, e) => {
+  e.stopPropagation();
+  try {
+    debugger;
+    let option_text = elem.innerText;
+    let option_value = elem.getAttribute("value");
+    let selected_items = elem.parentElement.parentElement.querySelector(
+      ".multiselect_fields"
+    );
+
+    let parentElement = document.createElement("div");
+    parentElement.setAttribute("class", "multiselect_field");
+    let childElement = document.createElement("div");
+    childElement.setAttribute("value", option_value);
+    childElement.setAttribute("class", "selected_content");
+    childElement.style.cssText = `color:${
+      selected_items.childElementCount % 2 === 0 ? "chocolate" : "crimson"
+    }`;
+    childElement.innerText = option_text;
+    let secondchildElement = document.createElement("div");
+    secondchildElement.setAttribute("class", "del_icon");
+
+    secondchildElement.innerHTML = "&#10006";
+    secondchildElement.addEventListener("click", function() {
+      return remove_selected_item(this, event);
+    });
+    parentElement.appendChild(childElement);
+
+    parentElement.appendChild(secondchildElement);
+
+    selected_items.appendChild(parentElement);
+
+    debugger;
+    elem.remove();
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+// remove the items
+const remove_selected_item = (elem, e) => {
+  e.stopPropagation();
+  debugger;
+  try {
+    let option_text = elem.parentElement.querySelector(".selected_content")
+      .innerHTML;
+    let option_value = elem.parentElement
+      .querySelector(".selected_content")
+      .getAttribute("value");
+    let selector = elem.parentElement.parentElement.parentElement.querySelector(
+      "#multi-select"
+    );
+
+    let listElement = document.createElement("li");
+
+    listElement.appendChild(document.createTextNode(option_text));
+    listElement.value = option_value;
+    listElement.addEventListener("click", function() {
+      return add_selected_item(this, event);
+    });
+    selector.appendChild(listElement);
+
+    elem.parentElement.remove();
+  } catch (e) {
+    console.error(e);
+  }
+};
